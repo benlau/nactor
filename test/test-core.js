b6c86247492b3f0916a594bc95f8bd5b80b2dad5
Ben Lau <xbenlau@gmail.com>
2013-04-04 01:38:58 +0800

    Added state concept in actor

--- test/test-core.js
+++ test/test-core.js
@@ -276,3 +276,70 @@ exports.event = function(test) {
 
     timeout(test);
 }
+
+exports.loadTesing = function(test) {
+	var max = 1000;
+	var pingCount = 0;
+	var helloCount = 0;
+    var actor = nactor.actor({
+        ping: function(data,async){
+			var actor = this;
+			async.enable();
+			pingCount++;
+			var timer = Math.floor((Math.random()*10)+1);
+			setTimeout(function(){
+				actor.emit("pong",pingCount);
+				async.reply();			
+			},timer);
+        },
+        
+        hello: function(data,async){
+			async.enable();
+			helloCount++;
+			var timer = Math.floor((Math.random()*10)+1);
+			setTimeout(function(){
+				async.reply();			
+			},timer);
+			
+			if (helloCount == max) {
+				test.equal(pingCount , max);
+				test.equal(helloCount , max);
+				test.done();				
+			}
+			
+        },
+        
+        final : function(){
+		}
+    });
+    
+	actor.init();
+	
+	actor.on("pong",function() {
+		if (pingCount % 2 == 0) {
+			actor.hello();
+		} else {
+			var timer = Math.floor((Math.random()*10)+1);
+			setTimeout(function(){
+				actor.hello();
+			},timer);
+		}		
+	});
+	
+	var i = 0;
+	
+	var handler = setInterval(function(){
+		var c = i + Math.floor((Math.random()*100)+1);
+		while (i < max && i < c) {
+			actor.ping();
+			i++;
+		}
+		if (i >=max){
+			clearInterval(handler);
+		}
+		
+	},50);
+	
+	
+	timeout(test,20 * 1000);
+}
