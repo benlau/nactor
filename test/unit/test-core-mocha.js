c2857846f3670efaa1aea30732cc244d797df07a
JediMindtrick <brandonjwilhite@gmail.com>
2015-04-18 13:21:20 -0400

    all tests moved over to mocha

--- test/unit/test-core-mocha.js
+++ test/unit/test-core-mocha.js
@@ -196,4 +196,332 @@ describe('core-tests',function(){
         });
 
     });
+
+    it('tests the ability to stop an actor after exceptions',function(done){
+
+        var actor = nactor.actor(function(options){
+            this.seq = 0;
+
+            return {
+                normal: function() {
+                    return {
+                        seq : ++this.seq
+                    }
+                },
+                fail : function() {
+                    var target;
+                    target.exec(); // It will throw exception.
+                }
+            }
+        });
+
+        actor.init();
+
+        actor.onUncaughtException(function(err,action){
+            expect(action).to.not.be.undefined;
+            action.stop();
+            setTimeout(function(){
+                done();
+            },200);
+        });
+
+        actor.normal(function(reply){
+            expect(reply.seq).to.equal(1);
+        });
+
+        actor.normal(function(reply){
+            expect(reply.seq).to.equal(2);
+        });
+
+        actor.fail(function(reply){
+            assert.fail("If actor's method throw exception, it should not return");
+        });
+
+        actor.normal(function(reply){
+            assert.fail("Expected fail");
+            done();
+        });
+
+    });
+
+    it('tests ask with non-object argument',function(done){
+        var actor = nactor.actor({
+            ping: function(msg){
+                return msg;
+            }
+        });
+
+        actor.init();
+
+        actor.ping("hello",function(msg){
+            expect(msg).to.equal("hello");
+            done();
+        });
+
+    });
+
+    it('tests events',function(done){
+        var actor = nactor.actor({
+            ping: function(msg){
+                var self = this;
+                setTimeout(function(){
+                    self.emit("pong",msg);
+                },300);
+            }
+        });
+
+        actor.init();
+
+        actor.ping("Hello!",function(reply){
+            expect(reply).to.be.undefined;
+        });
+
+        actor.on("pong",function(msg){
+            expect(msg).to.equal("Hello!");
+            done();
+        });
+    });
+
+    it('tests post anonymous',function(done){
+        var actor = nactor.actor(function() {
+            var self = this;
+
+            setTimeout(function(){
+                // Post an anonymouse function
+                self.post(function() {
+                    expect(self.actor()._state).to.equal("PROCESSING");
+                    expect(self.actor()._queue.length).to.equal(0);
+
+                    setTimeout(function() {
+                        expect(self.actor()._state).to.equal("IDLE");
+                        done();
+                    },100);
+
+                });
+            },100);
+
+            return { // An actor without any method.
+            }
+        });
+
+        actor.init();
+    });
+
+    it('tests post',function(done){
+        var actor = nactor.actor(function() {
+            var self = this;
+
+            setTimeout(function(){
+                self.post("ping","Hello!",function(data,async) {
+                    async.enable();
+                    expect(data).to.equal("pong");
+                    expect(self.actor()._state).to.equal("PROCESSING"); // The reply is also an anonymous function.
+
+                    setTimeout(function() {
+                        async.reply(); // Return without receiver
+
+                        setTimeout(function() {
+                            // IDLE after reply.
+                            expect(self.actor()._state).to.equal("IDLE");
+                            done();
+                        },100);
+                    },100);
+                });
+            },100);
+
+            return {
+                ping : function(msg) {
+                    expect(msg).to.equal("Hello!");
+                    return "pong"
+                }
+            }
+        });
+
+        actor.init();
+
+    });
+
+    it('tests next',function(done){
+        var seq = [];
+        var actor = nactor.actor(function() {
+
+            return {
+                a : function(msg) {
+                    expect(msg).to.equal("Hello!!");
+                    seq.push('a');
+                    this.next("b","Called by a"); // Inject 'b' into the queue
+                },
+                b : function(msg) {
+                    expect(msg).to.equal("Called by a");
+                    expect(seq.length).to.equal(1);
+                    expect(seq[0]).to.equal('a');
+                    seq.push('b');
+                },
+                c: function(msg) {
+                    expect(seq.length).to.equal(2);
+                    expect(seq[1]).to.equal('b');
+                    done();
+                }
+            }
+        });
+
+        actor.init();
+        actor.a("Hello!!");
+        actor.c();
+
+    });
+
+    it('tests onReceived',function(done){
+        var actor = nactor.actor({
+            blocked : function(time,async){
+                async.enable();
+                setTimeout(function() {
+                    async.reply();
+                },time);
+            },
+            nonblocked : function() {
+                return;
+            }
+        });
+
+        actor.init();
+
+        var name = "blocked";
+
+        actor.system.on("received",function(message) {
+            expect(message).to.not.be.undefined;
+            expect(message.name()).to.equal(name);
+        });
+
+        actor.system.on("completed",function(message) {
+            expect(message).to.not.be.undefined;
+            expect(message.name()).to.equal(name);
+        });
+
+        actor.blocked(200,function(){
+            setTimeout(function() {
+                name = "nonblocked";
+                actor.nonblocked(function() {
+                    setTimeout(function() {
+                        done();
+                    },100);
+                });
+
+            },100);
+        });
+
+    });
+
+    it('tests drama',function(done){
+        // The argument of ask should be compatible with drama.
+        var actor = nactor.actor({
+            wait : function(time,async){
+                async.enable();
+                setTimeout(function() {
+                    async.reply();
+                },time);
+            }
+        });
+
+        actor.init();
+
+        actor.ask(actor,"wait",200,function() {
+            done();
+        });
+    });
+
+    it('tests dispose',function(done){
+        var actor = nactor.actor({
+            blocked : function(time,async){
+                async.enable();
+                setTimeout(function() {
+                    async.reply();
+                },time);
+            },
+            nonblocked : function() {
+                return;
+            }
+        });
+
+        actor.init();
+
+        var name = "blocked";
+
+        actor.system.on("received",function(message) {
+            expect(message).to.not.be.undefined,
+            expect(message.name()).to.equal(name);
+
+            setTimeout(function() {
+                message.dispose();
+            },50);
+
+        });
+
+        actor.system.on("completed",function(message) {
+            assert.fail();// should not reach here
+        });
+
+        actor.system.on("disposed",function(message) {
+            expect(message).to.not.be.undefined;
+            expect(message.name()).to.equal(name);
+        });
+
+        actor.blocked(200,function(){
+            assert.fail();
+        });
+
+        setTimeout(function() {
+            done();
+        },300);
+    });
+
+    it('orders sequence of message consumption deterministically',function(done){
+        var res = [];
+        var actor;
+
+        actor = nactor.actor({
+            delayReply: function(data,async) {
+                async.enable();
+                setTimeout(function() {
+                    expect(data.index).to.not.be.undefined;
+                    res.push(data.index);
+                    async.reply();
+                    async.reply();
+                },data.timeout);
+            },
+            final: function() {
+            }
+        });
+
+        actor.init();
+
+        actor.onUncaughtException(function(err,action) {
+            expect(action).to.not.be.undefined;
+            expect(action.stop).to.not.be.undefined;
+        });
+
+        actor.delayReply({
+            index : 0,
+            timeout : 500
+        });
+
+        actor.delayReply({
+            index : 1,
+            timeout : 200
+        });
+
+        actor.delayReply({
+            index : 2,
+            timeout : 10
+        });
+
+        actor.final(function() {
+            expect(res.length).to.equal(3);
+            expect(res[0]).to.equal(0);
+            expect(res[1]).to.equal(1);
+            expect(res[2]).to.equal(2);
+            done();
+        });
+
+    });
 });
